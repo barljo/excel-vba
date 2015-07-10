c2e6f5e12ae19b56d7e2d8eec0397a855b2847b8
barljo <johnbarlow.iom@gmail.com>
2015-07-10 13:44:09 +0100

    Delete JsBattery.js

--- JsBattery.js
+++
@@ -1,8 +0,0 @@
-/*
-	requires Talk2Watch Pro 30.0.3.4
-*/
-
-var plaform = new PlatformInterface();
-var batteryLevel = plaform.getBatteryLevel();
-
-Talk2Watch.sendSms(String(batteryLevel) + " %", "[Battery]");
